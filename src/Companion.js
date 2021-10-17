f96ec5e83f3297c51ef49bbb064d36c7e8c600c0
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-10-16 21:22:04 -0400

    completed centering css with html tags

--- src/Companion.js
+++ src/Companion.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
+import './Companion.css'
 
 const Companion = () => {
     const [messages, setMessages] = useState([]);
@@ -9,13 +10,20 @@ const Companion = () => {
     return (
         <div>
             <h1>Companion</h1>
-            <Form>
-            <Form.Group className="mb-3 message-input" controlId="exampleForm.ControlTextarea1">
+            <div className="messages-view-container">
+                <div className="messages-view">
+                    <div className="message">
+                        <h1>Hello</h1>
+                    </div>
+                </div>
+            </div>
+            <Form className="message-input">
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                 <Form.Control as="textarea" placeholder="Type message here" rows={3} />
             </Form.Group>
             </Form>
         </div>
-    )
+    );
 }
 
 export default Companion;
