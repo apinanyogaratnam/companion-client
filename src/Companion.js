d7439c03a4fbbfdb9a2779bd8ecd7fcef1fd23c8
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-10-16 20:49:06 -0400

    created rafce

---
+++ src/Companion.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const Companion = () => {
+    return (
+        <div>
+            <h1>Companion</h1>
+        </div>
+    )
+}
+
+export default Companion;
