add884d3a1e802bee829cc72b74ecce3f53535cb
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-10-16 20:56:30 -0400

    added message states and form

--- src/Companion.js
+++ src/Companion.js
@@ -1,9 +1,19 @@
 import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
 
 const Companion = () => {
+    const [messages, setMessages] = useState([]);
+    // {bot: "text"}
+    // {user: "text"}
+
     return (
         <div>
             <h1>Companion</h1>
+            <Form>
+            <Form.Group className="mb-3 message-input" controlId="exampleForm.ControlTextarea1">
+                <Form.Control as="textarea" placeholder="Type message here" rows={3} />
+            </Form.Group>
+            </Form>
         </div>
     )
 }
